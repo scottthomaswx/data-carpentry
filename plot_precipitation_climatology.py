f9d935a550e39714550e280608b1706ee029692a
Scott Thomas <scottrhthomas@outlook.com>
2021-05-26 15:19:53 -0400

    Add land/ocean masking

--- plot_precipitation_climatology.py
+++ plot_precipitation_climatology.py
@@ -53,6 +53,22 @@ def create_plot(clim, model, season, gridlines=False, levels=None):
     title = f'{model} precipitation climatology ({season})'
     plt.title(title)
 
+def apply_mask(darray, sftlf_file, realm):
+    """ Mask Ocean or Land using a SFTLF (surface land fraction) file
+        args:
+        darray(xarray.DataArray): Data to mask
+        sftlf_file (str): Land surface fraction file
+        realm (str): Realm to mask
+        """
+    dset = xr.open_dataset(sftlf_file)
+
+    if realm == 'land':
+        masked_darray = darray.where(dset['sftlf'].data < 50)
+    else:
+        masked_darray = darray.where(dset['sftlf'].data > 50)
+
+    return masked_darray
+
 
 def main(inargs):
     """Run the program."""
@@ -61,6 +77,10 @@ def main(inargs):
     
     clim = dset['pr'].groupby('time.season').mean('time', keep_attrs=True)
     clim = convert_pr_units(clim)
+
+    if inargs.mask:
+        sftlf_file,realm = inargs.mask
+        clim = apply_mask(clim, sftlf_file, realm)
 
     create_plot(clim, dset.attrs['source_id'], inargs.season,
                 gridlines=inargs.gridlines, levels=inargs.cbar_levels)
@@ -79,6 +99,9 @@ if __name__ == '__main__':
                         help="Include gridlines on the plot")
     parser.add_argument("--cbar_levels", type=float, nargs='*', default=None,
                         help='list of levels / tick marks to appear on the colorbar')
+    parser.add_argument("--mask", type=str, nargs=2,
+                    metavar=('SFTLF_FILE', 'REALM'), default=None,
+                    help="""Provide sftlf file and realm to mask ('land' or 'ocean')""")
 
     args = parser.parse_args()
    
